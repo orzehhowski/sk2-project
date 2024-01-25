90ed7938ad42cb54ae7c420407915aae66240b27
orzehhowski <filmyminecraftnayt@gmail.com>
2024-01-25 13:40:02 +0100

    done I guess

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # sk2-project
 
+Prosta konsola do wykonywania shellowych poleceń na zdalnym urządzeniu, na którym został uruchomiony `server.py`. Po nawiązaniu połączenia poprzez uruchomienie `client.py` klient proszony jest o podanie nazwy użytkownika i hasła, które zostanie wykorzystane do próby autentykacji na konto systemowe systemu na którym odpalony jest serwer. Następnie użytkownik może wpisywać komendy niewymagające uprawnień administratora oraz niepytające o kolejny input. Programy działają na systemie Linux z zainstalowanym Pythonem.
+
 ### how to run
 
-In project directory (works only in linux environment):
+w terminalu w głównym folderze projektu:
 
 ```
 python3 -m venv venv
@@ -11,7 +13,7 @@ pip install -r ./requirements.txt
 python3 ./server.py
 ```
 
-In second terminal (ofc also in project dir):
+i w kolejnych terminalach, które będą klientami (również w głównym folderze):
 
 ```
 source ./venv/bin/activate
--- server.py
+++ server.py
@@ -1,26 +1,30 @@
 import socket
 import subprocess
 import threading
+import os
 import pam
-from pexpect import spawn, EOF
 
 # tries to authenticate linux user using pan
-def authenticate_user(username, password):
+def authenticate_user(username, password, id):
     try:
         if pam.authenticate(username, password, service='login'):
-            print(f"User {username} authenticated successfully")
+            log(id, f"User {username} authenticated successfully")
             return True
         else:
-            print(f"Authentication failed.")
+            log(id, f"Authentication failed.")
             return False
     except Exception as e:
-        print(f"Authentication failed: {e}")
+        log(id, f"Authentication failed: {e}")
         return False
+    
+def log(connection_id, message):
+    print(f"[{connection_id}] {message}")
 
 # Handles incoming client data and executes commands.
-def handle_client(client_socket):
+def handle_client(client_socket, id):
     client_socket.send(b"Welcome to the Remote Command Execution Console!\nUsername:\n")
     username = client_socket.recv(1024).decode("utf-8")
+    password = ""
 
     # authentication loop
     while True:
@@ -32,8 +36,8 @@ def handle_client(client_socket):
             if not password:
                 client_socket.send(b"authentication failed. Try again\nUsername:\n")
             else:
-                if authenticate_user(username, password):
-                    client_socket.send(f"User {username} authenticated\n>>>".encode("utf-8"))
+                if authenticate_user(username, password, id):
+                    client_socket.send(f"User {username} authenticated - type command or \"exit\" to terminate connection\n>>>".encode("utf-8"))
                     break
                 else:
                     client_socket.send(b"authentication failed. Try again\nUsername:\n")
@@ -46,13 +50,25 @@ def handle_client(client_socket):
         if (not command) or command == "exit":
             break
 
-        try:
-            # Execute the command using subprocess
-            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-            response = result
-        except subprocess.CalledProcessError as e:
-            # If the command execution fails, send the error message
-            response = f"Error: {e.output}"
+        # Change directory command
+        if command.split(" ")[0] == "cd":
+            directory = command[3:]
+            try:
+                os.chdir(directory)
+                response = f"Changed directory to: {os.getcwd()}\n"
+            except FileNotFoundError as e:
+                response = e.strerror + "\n"
+
+        else:
+            try:
+                # Execute the command using subprocess
+                response = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+
+            except subprocess.CalledProcessError as e:
+                # If the command execution fails, send the error message
+                response = f"Error: {e.output}"
+
+        log(id, f"Command output:\n{response}")
 
         # Send the command output or error back to the client
         response += ">>> "
@@ -60,6 +76,7 @@ def handle_client(client_socket):
 
     # Close the client socket when the connection is terminated
     client_socket.close()
+    log(id, "Connection closed.")
 
 def start_server():
     # Define the host and port to bind the server to
@@ -76,14 +93,17 @@ def start_server():
     server_socket.listen(5)
     print(f"[*] Listening on {host}:{port}")
 
+    connection_num = 1
+
     while True:
         # Accept a connection from a client
         client_socket, addr = server_socket.accept()
-        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
+        print(f"[*] Accepted connection from {addr[0]}:{addr[1]}. Connection id: {connection_num}")
 
         # Create a new thread to handle the client
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,connection_num))
         client_handler.start()
+        connection_num += 1
 
 if __name__ == "__main__":
     start_server()
